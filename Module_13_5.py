1af8e91bb61f4e6907b4e8ccb0be90a07c9159cc
83metra <jumo_004@mail.ru>
2024-09-30 21:45:59 +0200

    Create Module_13_5.py

---
+++ Module_13_5.py
@@ -0,0 +1,78 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+import asyncio
+
+api = ''
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+button1 = KeyboardButton(text='Рассчитать')
+button2 = KeyboardButton(text='Информация')
+kb.row(button1, button2)
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+    sex = State()
+
+
+@dp.message_handler(commands='start')
+async def start(message):
+    await message.answer('Привет! Я бот, помогающий твоему здоровью. Кстати, курение вредит оному!', reply_markup=kb)
+
+
+@dp.message_handler(text='Рассчитать')
+async def set_age(message):
+    await message.answer('Введите свой возраст.')
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=message.text)
+    date = await state.get_data()
+    await message.answer('Введите свой рост.')
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=message.text)
+    date = await state.get_data()
+    await message.answer('Введите свой вес')
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def set_sex(message, state):
+    await state.update_data(weight=message.text)
+    date = await state.get_data()
+    await message.answer('Введите свой пол (мужчина/женщина)')
+    await UserState.sex.set()
+
+
+@dp.message_handler(state=UserState.sex)
+async def send_calories(message, state):
+    await state.update_data(sex=message.text)
+    date = await state.get_data()
+    await message.answer(f"Возраст: {date['age']}, рост: {date['growth']},"
+                         f"  вес: {date['weight']}, пол: {date['sex']}")
+    if 'му' in str(date['sex']).lower():
+        result = (int(date['weight']) * 10) + (6.25 * int(date['growth'])) - (int(date['age']) + 5)
+        await message.answer('Ваша норма калорий: %s' % (result))
+    elif 'же' in str(date['sex']).lower():
+        result = (int(date['weight']) * 10) + (6.25 * int(date['growth'])) - (int(date['age']) - 161)
+        await message.answer('Ваша норма калорий: %s' % (result))
+    else:
+        await message.answer('В нашей стране только два пола — мужчина и женщина! Начните всё с начала!')
+    await state.finish()
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
